b040aee43632de19943eae47410a68f896ea2330
boeing7478F <85016918+boeing7478F@users.noreply.github.com>
2021-05-29 17:21:41 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
